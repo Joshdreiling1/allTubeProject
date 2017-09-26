e5d7eb790c5463ed2dee91a6812ebc093e007f7e
Josh Dreiling <josh.dreiling77@gmail.com>
2017-09-25 22:19:07 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+import searchlogo from './assets/searchlogo.png'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+        <h2 className="Header-title">allTube</h2>
+        <button className='signup'> Sign in or Sign up </button>
+        </div>
+        
+        <div className='main'>
+           <div className='upload'>
+
+             </div>
+           <div className='searchbox'>
+
+             </div>
+
+
+
+          </div>
+
+   
+
+        </div>
+    );
+  }
+}
+
+export default App;
